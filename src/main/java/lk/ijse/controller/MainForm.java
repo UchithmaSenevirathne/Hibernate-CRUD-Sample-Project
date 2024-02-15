b8e0bd633d6e3a23f9fb5707954ca3ca1538fb1f
UchithmaSenevirathne <uchithma@gmail.com>
2024-02-15 10:19:17 +0530

    update main form

--- src/main/java/lk/ijse/controller/MainForm.java
+++ src/main/java/lk/ijse/controller/MainForm.java
@@ -1,4 +1,30 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class MainForm {
+    @FXML
+    private AnchorPane anchorpane;
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
 }

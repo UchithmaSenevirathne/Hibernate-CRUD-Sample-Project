b819e526d2773e274898430f6a04cd45a290eaff
UchithmaSenevirathne <uchithma@gmail.com>
2024-02-15 10:42:55 +0530

    update appInitializer

--- src/main/java/lk/ijse/AppInitializer.java
+++ src/main/java/lk/ijse/AppInitializer.java
@@ -1,7 +1,20 @@
 package lk.ijse;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Main.fxml"))));
+        stage.setTitle("WELCOME");
+        stage.centerOnScreen();
+        stage.show();
     }
 }
